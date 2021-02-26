a651ad99556ce402a5142c7f4163660342ec88e1
Stephen Burnett <stephenburnett@Stephens-MBP.fios-router.home>
2021-02-25 20:30:13 -0500

    addded module for webpack5

--- next.config.js
+++ next.config.js
@@ -2,8 +2,12 @@ const withSass = require('@zeit/next-sass');
 const withImages = require('next-images');
 const withLess = require('@zeit/next-less')
 
-module.exports = withCSS(withLess(withImages(withSass({
+module.exports = withLess(withImages(withSass({
   env: {
     ANY_ENV_KEY: "ANY_ENV_VARIABLE"
   }
-}))));
+})));
+
+module.exports = {
+    future: { webpack5: true }
+  }
