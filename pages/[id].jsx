2249a2eeb5ef69c680f93742af0dea19946c9d39
Stephen Burnett <stephenburnett@Stephens-MBP.fios-router.home>
2021-02-25 20:06:41 -0500

    added favicon

--- pages/[id].jsx
+++ pages/[id].jsx
@@ -1,57 +1,135 @@
 import pokemonJSON from "./api/pokemonData.json";
 import { ProgressBar, Col, Row, Container, Button } from "react-bootstrap";
 import Link from "next/link";
-import Image from "next/image"
+import Image from "next/image";
+import Head from "next/head";
 
 export default function id({ pokemon }) {
-    let maxHP = 255
-    let maxAttack = 180
-    let maxDefense = 230
-    let maxSpAttack = 180
-    let maxSpDefense = 230
-    let maxSpeed = 180
+  let maxHP = 255;
+  let maxAttack = 180;
+  let maxDefense = 230;
+  let maxSpAttack = 180;
+  let maxSpDefense = 230;
+  let maxSpeed = 180;
   return (
-    <Container>
-        <Link href="/"><div className="flex justify-center logo-spacing"><Image  height={200} width={500} src="/images/pokemon-logo.png"></Image></div></Link>
-      <div className="capital-first pokemon-font pokemon-header text-center">
-        {pokemon.name}
-      </div>
-
-      <div className="flex flex-row justify-center">
-      <Row   xs={1} sm={2}>
-        <Col className="flex space-around">
-        <div className="flex flex-col place-content-center">
-          <img height={250} width={250} src={pokemon.image} />
-        
-        <div className="flex justify-center flex-col">
-            <div className="text-center">{pokemon.type[0]}{pokemon.type[1] ? `, ${pokemon.type[1]}` : ""}</div>
-            <div className="flex justify-center flex-row">
-          <Image height={50} width={50} src={`/images/${pokemon.type[0]}.png`} />
-          {pokemon.type[1] ? <Image height={30} width={30} src={`/images/${pokemon.type[1]}.png`} /> : ""}
+    <div>
+      <Head>
+        <title className="capital-first">{pokemon.name}</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Container>
+        <Link href="/">
+          <div className="flex justify-center logo-spacing">
+            <Image
+              height={200}
+              width={500}
+              src="/images/pokemon-logo.png"
+            ></Image>
           </div>
-          </div>
-          </div>
-        </Col>
-        <Col>
-        <div className="flex flex-col justify-center py-2">
-          <div className="text-center pokemon-font">Stats</div>
-          <div className="py-2">{`HP: ${pokemon.base.HP}/${maxHP}`} <ProgressBar striped variant="success" now={pokemon.base.HP} max={maxHP}/></div>
-          
-          <div className="py-2">{`Attack: ${pokemon.base.Attack}/${maxAttack}`}<ProgressBar striped variant="danger" now={pokemon.base.Attack} max={maxAttack}/></div>
-          <div className="py-2">{`Defense: ${pokemon.base.Defense}/${maxDefense}`}<ProgressBar striped variant="primary" now={pokemon.base.Defense} max={maxDefense}/></div>
-          <div className="py-2">{`Special Attack ${pokemon.base.SpAttack}/${maxSpAttack}`}<ProgressBar striped variant="secondary" now={pokemon.base.SpAttack} max={maxSpAttack}/></div>
-          <div className="py-2">{`Special Defense ${pokemon.base.SpDefense}/${maxSpDefense}`}<ProgressBar striped variant="info" now={pokemon.base.SpDefense} max={maxDefense}/></div>
-          <div className="py-2">{`Speed: ${pokemon.base.Speed}/${maxSpeed}`} <ProgressBar striped variant="warning" now={pokemon.base.Speed} max={maxSpeed}/></div>
-        </div>
-        </Col>
-        </Row>
-      </div>
-      <div className="flex justify-center">
-        <Link href="/" className="text-center flex justify-items-center">
-          <Button>Back</Button>
         </Link>
-      </div>
-    </Container>
+        <div className="capital-first pokemon-font pokemon-header text-center">
+          {pokemon.name}
+        </div>
+
+        <div className="flex flex-row justify-center">
+          <Row xs={1} sm={2}>
+            <Col className="flex space-around">
+              <div className="flex flex-col place-content-center">
+                <img height={300} width={300} src={pokemon.image} />
+
+                <div className="flex justify-center flex-col">
+                  <div className="text-center">
+                    {pokemon.type[0]}
+                    {pokemon.type[1] ? `, ${pokemon.type[1]}` : ""}
+                  </div>
+                  <div className="flex justify-center flex-row">
+                    <Image
+                      height={50}
+                      width={50}
+                      src={`/images/${pokemon.type[0]}.png`}
+                    />
+                    {pokemon.type[1] ? (
+                      <Image
+                        height={50}
+                        width={50}
+                        src={`/images/${pokemon.type[1]}.png`}
+                      />
+                    ) : (
+                      ""
+                    )}
+                  </div>
+                </div>
+              </div>
+            </Col>
+            <Col>
+              <div className="flex flex-col justify-center py-2">
+                <div className="text-center pokemon-font pt-4">Stats</div>
+                <div className="py-2">
+                  {`HP: ${pokemon.base.HP}/${maxHP}`}{" "}
+                  <ProgressBar
+                    striped
+                    variant="success"
+                    now={pokemon.base.HP}
+                    max={maxHP}
+                  />
+                </div>
+
+                <div className="py-2">
+                  {`Attack: ${pokemon.base.Attack}/${maxAttack}`}
+                  <ProgressBar
+                    striped
+                    variant="danger"
+                    now={pokemon.base.Attack}
+                    max={maxAttack}
+                  />
+                </div>
+                <div className="py-2">
+                  {`Defense: ${pokemon.base.Defense}/${maxDefense}`}
+                  <ProgressBar
+                    striped
+                    variant="primary"
+                    now={pokemon.base.Defense}
+                    max={maxDefense}
+                  />
+                </div>
+                <div className="py-2">
+                  {`Special Attack ${pokemon.base.SpAttack}/${maxSpAttack}`}
+                  <ProgressBar
+                    striped
+                    variant="secondary"
+                    now={pokemon.base.SpAttack}
+                    max={maxSpAttack}
+                  />
+                </div>
+                <div className="py-2">
+                  {`Special Defense ${pokemon.base.SpDefense}/${maxSpDefense}`}
+                  <ProgressBar
+                    striped
+                    variant="info"
+                    now={pokemon.base.SpDefense}
+                    max={maxDefense}
+                  />
+                </div>
+                <div className="py-2">
+                  {`Speed: ${pokemon.base.Speed}/${maxSpeed}`}{" "}
+                  <ProgressBar
+                    striped
+                    variant="warning"
+                    now={pokemon.base.Speed}
+                    max={maxSpeed}
+                  />
+                </div>
+              </div>
+            </Col>
+          </Row>
+        </div>
+        <div className="flex justify-center py-2">
+          <Link href="/" className="text-center flex justify-items-center">
+            <Button>Back</Button>
+          </Link>
+        </div>
+      </Container>
+    </div>
   );
 }
 
--- pages/index.js
+++ pages/index.js
@@ -1,79 +1,82 @@
-import Head from 'next/head'
-import { useState } from 'react'
-import allPokemon from "./api/pokemonData.json"
-import Image from "next/image"
-import Link from "next/link"
-import { FormControl, Card, Col, Row, Container } from "react-bootstrap"
-
-
+import Head from "next/head";
+import { useState } from "react";
+import allPokemon from "./api/pokemonData.json";
+import Image from "next/image";
+import Link from "next/link";
+import { FormControl, Card, Col, Row, Container } from "react-bootstrap";
 
 export default function Home() {
+  let [nameSearch, setNameSearch] = useState("");
 
-let [nameSearch, setNameSearch] = useState("")
-// let [nameSearchPokemon, setNameSearchPokeon] = useState()
+  let handleChange = (e) => {
+    setNameSearch(e.target.value);
+  };
 
-    let handleChange = e => {
-      setNameSearch(e.target.value);
-    };
-
-  let searchPokemon = allPokemon.filter((pokemon) => pokemon.name.toLowerCase().includes(nameSearch.toLowerCase()))
-
+  let searchPokemon = allPokemon.filter((pokemon) =>
+    pokemon.name.toLowerCase().includes(nameSearch.toLowerCase())
+  );
 
   return (
     <div>
-
       <Head>
         <title>Pokedex</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <Container>
-        <div className="flex justify-center logo-spacing"><Image  height={200} width={500} src="/images/pokemon-logo.png"></Image></div>
-        <FormControl 
-        placeholder="Search"
-        onChange={handleChange}
-        />
+        <div className="flex justify-center logo-spacing">
+          <Image
+            height={200}
+            width={500}
+            src="/images/pokemon-logo.png"
+          ></Image>
+        </div>
+        <FormControl placeholder="Search" onChange={handleChange} />
 
-      <Row  xs={2} sm={2} md={3} lg={4} xl={5}>
-      
-      {searchPokemon.map((pokemon, key) => (
-        <div key={key} className="my-2">
-<Col >
-<Link href={`/[id]?id=${pokemon.id}`} as={`/${pokemon.id}`}>
-          <Card>
-          
-          <Card.Img src={pokemon.image}/>
-        <Card.Text>#{pokemon.id}</Card.Text>
-        <Card.Title  className="text-center capital-first">{pokemon.name}</Card.Title>
-        <div className="flex flex-row justify-center">
-        <div >
-          <Image height={30} width={30} src={`/images/${pokemon.type[0]}.png`} />
-          {pokemon.type[1] ? <Image height={30} width={30} src={`/images/${pokemon.type[1]}.png`} /> : ""}
-          </div>
-        </div>
-        
-        </Card>
-        </Link>
-        </Col>
-        </div>
-       
-      ))}
-      
-      </Row>
+        <Row xs={2} sm={2} md={3} lg={4} xl={5}>
+          {searchPokemon.map((pokemon, key) => (
+            <div key={key} className="my-2">
+              <Col>
+                <Link href={`/[id]?id=${pokemon.id}`} as={`/${pokemon.id}`}>
+                  <Card>
+                    <Card.Img src={pokemon.image} />
+                    <Card.Text>#{pokemon.id}</Card.Text>
+                    <Card.Title className="text-center capital-first">
+                      {pokemon.name}
+                    </Card.Title>
+                    <div className="flex flex-row justify-center">
+                      <div>
+                        <Image
+                          height={30}
+                          width={30}
+                          src={`/images/${pokemon.type[0]}.png`}
+                        />
+                        {pokemon.type[1] ? (
+                          <Image
+                            height={30}
+                            width={30}
+                            src={`/images/${pokemon.type[1]}.png`}
+                          />
+                        ) : (
+                          ""
+                        )}
+                      </div>
+                    </div>
+                  </Card>
+                </Link>
+              </Col>
+            </div>
+          ))}
+        </Row>
       </Container>
-      
-      
-      
-
-
     </div>
-  )
+  );
 }
 
 // export const getServerSideProps = async cxt => {
 //   let  res = pokemonApi
 //   let allPokemon = res.slice(0, 809).map((pokemon, index) => {
-    
+
 //         let id = newApi[index].id
 //         let base = newApi[index].base
 //         let type = newApi[index].type
@@ -81,10 +84,9 @@ let [nameSearch, setNameSearch] = useState("")
 //           id,
 //           base,
 //           type,
-//           } 
+//           }
 // })
 // return {
 //   props: {allPokemon}
 // }
 // }
-
